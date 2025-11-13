2dc7de03cede724c1c94eab15d82f25837d1b99c
Damasus Okeke <gwill2be@gmail.com>
2025-11-13 06:43:06 +0100

    Initial commit

---
+++ HospitalJournalSystem copy.md
@@ -0,0 +1,6 @@
+# HospitalJournalSystem database
+Objects or tables: ```Patients```, ```Doctors```, ```Appointments```, and ```JournalData.```
+
+### The database will have the following relations:
+1:M-relation --> One Doctor can create many JournalData
+M:M-relation --> One Patient can see many Doctors, and One Doctor can treat many Patients.
---
+++ MOCK_DATA_doctor.sql
@@ -0,0 +1,9 @@
+INSERT INTO Doctors (DoctorID, FirstName, LastName, Email, Speciality) VALUES
+(1, 'Royal', 'Grahlman', 'royal.grahlman@kalixhospital.org', 'General_practice'),
+(2, 'Jarrett', 'Daulton', 'jarrett.daulton@kalixhospital.org', 'General_surgery'),
+(3, 'Mandi', 'Piatto', 'mandi.piatto2@kalixhospital.org', 'General_practice'),
+(4, 'Standford', 'Rought', 'standford.rought@kalixhospital.org', 'Cardiology'),
+(5, 'Judi', 'Jakobssen', 'judi.jakobssen@kalixhospital.org', 'General_practice'),
+(6, 'Krysta', 'Klimschak', 'krysta.klimschak@kalixhospital.org', 'General_surgery'),
+(7, 'Celie', 'Cheverton', 'celie.cheverton@kalixhospital.org', 'General_practice')
+(8, 'Odey', 'Shackle', 'odey.shackle@kalixhospital.org', 'Anaesthetics');
